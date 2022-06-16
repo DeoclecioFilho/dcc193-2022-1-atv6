d094d7115ac6ceb7ebb41de37e6245b230bdbeb2
DeoclecioFilho <deoclecio.filho@estudante.ufjf.br>
2022-06-15 21:24:45 -0300

    criado os atividade-form, index e listar

--- tomato/src/main/java/br/ufjf/dcc193/tomato/Atividade.java
+++ tomato/src/main/java/br/ufjf/dcc193/tomato/Atividade.java
@@ -4,13 +4,17 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PositiveOrZero;
 
 @Entity
 public class Atividade {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    @NotBlank(message = "Digite um título!")
     private String titulo;
+    @PositiveOrZero(message="Digite 0 ou mais tomatos!")
     private Integer tomatos;
 
 
--- tomato/src/main/java/br/ufjf/dcc193/tomato/AtividadeController.java
+++ tomato/src/main/java/br/ufjf/dcc193/tomato/AtividadeController.java
@@ -2,17 +2,69 @@ package br.ufjf.dcc193.tomato;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 //import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+import java.util.List;
+
+
 
 @Controller
+@RequestMapping("/atividade")
 public class AtividadeController {
-    @Autowired
-    AtividadeRepository atividadeRepo;
-    
-    @RequestMapping({"/atividade","/atividade/index.html"})
-  //  @ResponseBody
-    public String index(){
-        return "atividade-index";
+  @Autowired
+  AtividadeRepository atividadeRepo;
+
+  @RequestMapping({ "/", "/index.html" })
+  // @ResponseBody
+  public String index() {
+    return "atividade-index";
+  }
+/* 
+  @RequestMapping(value = "/atividade/nova.html", method = RequestMethod.GET)
+  public ModelAndView criar() {
+    ModelAndView mv = new ModelAndView();
+    mv.setViewName("atividade-form");
+    mv.addObject("atividade", new Atividade(null, "Teste", null));
+    return mv;
+  }
+
+  @RequestMapping(value = "/atividade/nova.html", method = RequestMethod.POST)
+  public ModelAndView criar(Atividade atividade) {
+    ModelAndView mv = new ModelAndView();
+    mv.setViewName("redirect:listar.html");
+    atividadeRepo.save(atividade);
+    mv.addObject("atividade", new Atividade(null, "Teste", null));
+    return mv;
+  }
+*/
+   @GetMapping("/nova.html")
+   public ModelAndView criar() {
+     ModelAndView mv = new ModelAndView();
+     mv.setViewName("atividade-form");
+     mv.addObject("atividade", new Atividade(null, "Teste", null));
+     return mv;
     }
+    
+    @PostMapping("/nova.html")
+    public ModelAndView criar(Atividade atividade) {
+      ModelAndView mv = new ModelAndView();
+      mv.setViewName("redirect:listar.html");
+      mv.addObject("atividade", atividade);
+      return mv;
+    }
+    
+   
+  @GetMapping("/listar.html")
+  public ModelAndView listar() {
+    List<Atividade> atividades = atividadeRepo.findAll();
+    ModelAndView mv = new ModelAndView();
+    mv.setViewName("atividade-listar");
+    mv.addObject("atividade", atividades);
+    return mv;
+  }
+
 }
