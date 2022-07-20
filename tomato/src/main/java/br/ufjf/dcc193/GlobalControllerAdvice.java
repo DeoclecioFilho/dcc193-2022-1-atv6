97360a15768fe23ce5f33b41334e7ab488a2aa90
Deoclecio <deoclecio.filho@estudante.ufjf.br>
2022-07-19 23:00:49 -0300

    mensagem de erro

---
+++ tomato/src/main/java/br/ufjf/dcc193/GlobalControllerAdvice.java
@@ -0,0 +1,22 @@
+package br.ufjf.dcc193;
+
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@Component
+@ControllerAdvice
+public class GlobalControllerAdvice {
+    @ExceptionHandler
+    public ModelAndView trataDivisaoPorZero(NullPointerException e)
+    {
+        ModelAndView mv = new ModelAndView("error");
+        mv.addObject("status",500);
+        mv.addObject("error","Deu ruim");
+        mv.addObject("message","Não chame operações em null");
+        return mv;
+    }
+}
+
--- tomato/src/main/java/br/ufjf/dcc193/tomato/HomerController.java
+++ tomato/src/main/java/br/ufjf/dcc193/tomato/HomerController.java
@@ -1,7 +1,9 @@
 package br.ufjf.dcc193.tomato;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomerController {
@@ -11,10 +13,18 @@ public class HomerController {
         return "layout-padrao";
     }
     @GetMapping("/erro.html")
-    public String erro()
+    public String erro()throws Exception
     {
         //throw new Exception("Teste de erro");
         int a =10/0;
         return "layout-padrao";
+    }
+    @ExceptionHandler
+    public ModelAndView trataDivisaoPorZero(ArithmeticException e){
+        ModelAndView mv = new ModelAndView("error");
+        mv.addObject("status", "500");
+        mv.addObject("error","Deu ruim");
+        mv.addObject("message","Não divida por zero");
+        return mv;
     }
 }
